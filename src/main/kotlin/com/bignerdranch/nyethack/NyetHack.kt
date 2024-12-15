3ed09690f9a325748662d6114c910ac5be205229
Anton Cherepov <anton.cherepov@rabobank.nl>
2024-12-15 15:22:46 +0100

    move game singleton to separate file

--- src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
@@ -28,24 +28,3 @@ private fun promptHeroName(): String {
     println("Madrigal");
     return "Madrigal";
 }
-
-object Game {
-    private val currentRoom = Tavern();
-
-    init {
-        narrate("Welcome, adventurer!")
-
-        val mortality = if (player.isImmortal) "an immortal" else "a mortal";
-        narrate("${player.name}, $mortality, has ${player.healthPoints} health points");
-    }
-
-    fun play() {
-        while (true) {
-            narrate("${player.name} of ${player.hometown}, ${player.title}, is in ${currentRoom.description()}");
-            currentRoom.enterRoom();
-
-            print("> Enter your command: ");
-            println("Last command: ${readLine()}");
-        }
-    }
-}
