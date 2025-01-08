8a44f4c159f5e63a2c255e1ffd9bbc94fc395701
Anton Cherepov <anton.cherepov@rabobank.nl>
2025-01-09 00:02:53 +0100

    add creature interface and abstract class

--- src/main/kotlin/com/bignerdranch/nyethack/Player.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Player.kt
@@ -3,14 +3,18 @@ package com.bignerdranch.nyethack
 class Player(
     initialName: String,
     val hometown: String = "Neversummer",
-    var healthPoints: Int,
+    override var healthPoints: Int,
     val isImmortal: Boolean
-) {
-    var name = initialName
+) : Fightable {
+    override var name = initialName
         get() = field.replaceFirstChar { it.uppercase() }
         private set(value) {
             field = value.trim()
         }
+
+    override val diceCount = 3;
+
+    override val diceSides = 4;
 
     val title: String
     get() = when {
@@ -62,5 +66,11 @@ class Player(
     fun prophesize() {
         narrate("$name thinks about their future");
         narrate("A fortune teller told $name, \"$prophecy\"");
+    }
+
+    override fun takeDamage(damage: Int) {
+       if (!isImmortal) {
+           healthPoints -= damage;
+       }
     }
 }
